977d56bccb007a7446559d18011ee5097595c9cb
shreyansh-p <shreyansh.p@copperpoddigital.com>
2024-06-11 11:58:33 +0530

    [001][a-stop-the-time-app][shreyansh] : Two Way Binding Implemented
    Two way Binding implemented to set the player's name

--- a-stop-the-timmer-app/src/components/Player.jsx
+++ a-stop-the-timmer-app/src/components/Player.jsx
@@ -1,10 +1,24 @@
+import {useState} from "react";
+
 export default function Player() {
+
+  const [name, setName] = useState();
+  const [isSet, setIsSet] = useState(false);
+  function handleNameChange(event){
+      setIsSet(false);
+      setName(event.target.value);
+  }
+
+  function handleSetName(){
+      setIsSet(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome { !isSet ? 'Unknown' : name}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleNameChange} value={name} />
+        <button onClick={handleSetName}>Set Name</button>
       </p>
     </section>
   );
