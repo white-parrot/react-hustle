bec22b8f387cafd44e387d5bf312fe12e097006a
shreyansh-p <shreyansh.p@copperpoddigital.com>
2024-06-11 14:17:13 +0530

    [006][a-stop-the-time-app][shreyansh] : Functional Implementation done for TimeChallenge Component

--- a-stop-the-timmer-app/src/components/TimerChallenge.jsx
+++ a-stop-the-timmer-app/src/components/TimerChallenge.jsx
@@ -1,14 +1,35 @@
+import {useRef, useState} from "react";
+
 export default  function TimerChallenge({title, targetTime}){
+    let timer = useRef();
+    const [timeExpired, setTimeExpired] = useState();
+    const [timerStarted, setTimerStarted] = useState(false);
+
+    function handleStartTimer(){
+        timer.current = setTimeout( () => {
+            setTimeExpired(true);
+        }, targetTime * 1000);
+        setTimeExpired(false);
+        setTimerStarted(true);
+    }
+
+    function handleStopTimer(){
+        clearTimeout(timer.current);
+        setTimerStarted(false);
+        setTimeExpired(false);
+    }
+
     return (
         <section className="challenge">
             <h2>{title}</h2>
+            {timeExpired ? 'You Lost !' : ''}
             <p className="challenge-time">{targetTime} Second{targetTime > 1 ? 's' : ''}</p>
             <p>
-                <button>
-                    Start Challenge
+                <button onClick={timerStarted ? handleStopTimer : handleStartTimer}>
+                    {timerStarted ? 'Stop Timer' : 'Start Timer'}
                 </button>
             </p>
-            <p>Time is running... | Time Inactive</p>
+            <p>{timerStarted ? 'Time is running...' : 'Time Inactive'}</p>
         </section>
     );
 }
