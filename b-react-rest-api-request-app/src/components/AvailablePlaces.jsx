a871dd9a814e29573a8d237b570b1ca08cfdc2b6
white-parrot <everydaycodestory@gmail.com>
2024-06-18 20:18:59 +0530

    [012][b-react-rest-api-request-app][shreyansh] Get API Implemented using async/await and Loding State Implemented

--- b-react-rest-api-request-app/src/components/AvailablePlaces.jsx
+++ b-react-rest-api-request-app/src/components/AvailablePlaces.jsx
@@ -2,19 +2,23 @@ import Places from './Places.jsx';
 import {useEffect, useState} from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
   useEffect(() => {
-      fetch('http://localhost:3000/places')
-          .then((response) => {
-              return response.json();
-          }).then((jsonData) => {
-          setAvailablePlaces(jsonData.places);
-      })
+      setIsFetching(true)
+      async function fetchRecord() {
+          const response = await fetch('http://localhost:3000/places');
+          const record = await response.json();
+          setAvailablePlaces(record.places);
+          setIsFetching(false)
+      }
+      fetchRecord().then();
   }, []);
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
